bd7ba0444d2d3a71c45e0f9bc4631e5e47f6427d
kenshin-yamamoto <75126463+kenshin-yamamoto@users.noreply.github.com>
2021-01-16 15:35:01 +0900

    オブジェクトの削除の簡約化
    カメラの位置を取得して、カメラが通り過ぎたらオブジェクトが削除されるように変更

--- Assets/Scripts/ItemController.cs
+++ Assets/Scripts/ItemController.cs
@@ -4,25 +4,15 @@ using UnityEngine;
 
 public class ItemController : MonoBehaviour
 {
-    //Unityちゃんのオブジェクト
-    GameObject unitychan;
-
-    //Unityちゃんとアイテムの距離
-    private float difference;
 
     void Start()
     {
-        //Unityちゃんのオブジェクトを取得
-        this.unitychan = GameObject.Find("unitychan");
+
     }
 
     void Update()
     {
-        //アイテムとUnityちゃん(Z座標)の差を求める
-        this.difference = this.transform.position.z - this.unitychan.transform.position.z;
-
-        //もし差が-7以下になったらアイテムを破棄する
-        if (difference < -7.0f)
+        if(this.transform.position.z < Camera.main.transform.position.z)
         {
             Destroy(this.gameObject);
         }
